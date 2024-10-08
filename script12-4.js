abc6cf45bf92e4c7e199ac4d97125daf9cdc6ef2
RichardVanAlphen <111999466+RichardVanAlphen@users.noreply.github.com>
2024-10-08 13:05:58 +0200

    Add files via upload

---
+++ script12-4.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#btnLoad').on('click',function(){
+        const from = 'usd';
+        let to = 'eur';
+        let value = $('#txtDollar').val();
+        let exchangeUrl = 'https://openexchangerates.org/api/convert/' + value + '/' + from + '/' + to + '/' + 'app_id=a6f004f12f324b24b2f0372e15040de7';
+        $.ajax({
+            url: exchangeUrl,
+            success: function(exchange) {
+                console.log(exchange);
+                let html = 'Euro:' + exchange.eur + '.<br />';
+                    $('#divResult').append(html);
+            }
+        });
+    });
+}); 
